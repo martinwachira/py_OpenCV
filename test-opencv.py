366808755e6f3a7455de8e26a6965ab2afb48f0e
martin-wachira <martinnwachira@gmail.com>
2022-03-01 20:13:37 +0300

    learn opencv --1

--- test-opencv.py
+++ test-opencv.py
@@ -1,3 +1,19 @@
 import cv2 as cv
+import sys
 
-print('version =',cv.__version__)
+# print('version =',cv.__version__)
+
+img = cv.imread('deer.jpg')
+
+if img is None:
+    print('No found Image')
+    sys.exit('could not read the image')
+    
+cv.imshow('image',img)
+
+k = cv.waitKey(1000)
+
+if k == ord('s'):
+    cv.imwrite('deer_copy.jpg',img)
+    print('Image saved')
+    
